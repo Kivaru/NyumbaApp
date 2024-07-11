56a68199b40299ddd2031896409d3b1ca8b7c92c
Godfrey Kivaru <kivarugodfrey@gmail.com>
2024-07-11 12:58:11 +0300

    simple angular app for listing houses from json-server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <main>
+      <header class="brand-name">
+        <img style="width:40px;height:40px" class="brand-logo" src="./assets/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `,
+  imports: [RouterOutlet, HomeComponent, RouterModule],
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'NyumbaApp';
